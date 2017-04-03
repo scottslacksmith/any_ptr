524761dc7168bb713b158c35b846a515db2c257d
SSS-XPS-9530 <scott_slacksmith@hotmail.com>
2017-04-03 22:29:00 +0100

    Add any_shared_ptr

---
+++ include/any_shared_ptr.h
@@ -0,0 +1,285 @@
+#pragma once
+#include <memory>
+#include <typeinfo>
+#include <type_traits>
+#include <optional>
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+// Silence the 'unused-function' warning that first appeared when using GCC 6.2.
+// This warning wasn't emitted by GCC 4.9 so I suspect it's a false positive.
+#pragma GCC diagnostic ignored "-Wunused-function"
+#endif
+
+namespace xxx {
+
+  class bad_any_shared_ptr_cast : public std::bad_cast
+  {
+  public:
+    virtual const char* what() const noexcept override { return "bad any_shared_ptr cast"; }
+  };
+
+  inline namespace ver_1 {
+
+    class any_shared_ptr
+    {
+    public:
+      //-----------------------------------------------------
+      // Canonical "Rule of 5" ctors/dtor and copy operators
+
+      ~any_shared_ptr();
+
+      any_shared_ptr(any_shared_ptr const & other) noexcept;
+
+      any_shared_ptr(any_shared_ptr && other) noexcept;
+
+      any_shared_ptr& operator=(any_shared_ptr const& other) noexcept;
+
+      any_shared_ptr& operator=(any_shared_ptr && other) noexcept;
+
+      //-----------------------------------------------------
+      // ctors
+
+      template<typename T>
+      any_shared_ptr(std::shared_ptr<T> ptr) noexcept;
+
+      any_shared_ptr() noexcept;
+
+      //-----------------------------------------------------
+      // Modifiers
+
+      void swap(any_shared_ptr & other) noexcept;
+
+      // reset to empty state 
+      void reset() noexcept { ::new (this) any_shared_ptr(); }
+
+      //-----------------------------------------------------
+      // Observers
+
+      // return true if not empty
+      bool has_value() const noexcept { return my_type_info != nullptr; }
+
+      // Return the T's type-info for the held by shared_ptr<T>
+      const std::type_info & type() const noexcept;
+
+      // Return true if the held shared_ptr use_count is 1 (mimics std::shared_ptr::unique())
+      bool  unique() const noexcept;
+
+    private:
+
+      template<typename T>
+      using HeldBaseType = T;
+
+      template<typename T>
+      using HeldType = std::shared_ptr<T>;
+
+
+      template <typename T>
+      std::shared_ptr<T> cast(bool & cast_ok) const noexcept;
+
+      // Base class for the class holds shared_ptr
+      struct IHolder
+      {
+        virtual ~IHolder() = default;
+
+        virtual bool                    unique() const noexcept = 0;
+        virtual const IHolder *         clone(void* const inplaceMemory) const noexcept = 0;
+        virtual void                    try_up_cast_by_throwing() const = 0;
+        virtual std::shared_ptr<void>   make_shared_ptr_alias(const void * ptr) const noexcept = 0;
+        virtual std::shared_ptr<void>   make_shared_ptr_alias(const volatile void * ptr) const noexcept = 0;
+      };
+
+
+      template<typename T>
+      struct Holder final : public IHolder
+      {
+        Holder()
+          : my_ptr()
+        {}
+
+        Holder(std::shared_ptr<T> ptr)
+          : my_ptr(std::move(ptr))
+        {}
+
+        bool                    unique() const noexcept final { return my_ptr.unique(); }
+
+        const IHolder *         clone(void* const inplaceMemory) const noexcept final { return ::new (inplaceMemory) Holder(my_ptr); }
+
+        void                    try_up_cast_by_throwing() const final { throw my_ptr.get(); }
+
+        std::shared_ptr<void>   make_shared_ptr_alias(const void* p) const noexcept final 
+        { 
+          return std::shared_ptr<void>(my_ptr, const_cast<void*>(p));  
+        }
+
+        std::shared_ptr<void> make_shared_ptr_alias(const volatile void* p) const noexcept final 
+        { 
+          return std::shared_ptr<void>(my_ptr, const_cast<void*>(p)); 
+        }
+
+        // The held shared_ptr
+        std::shared_ptr<T>  my_ptr;
+
+      };
+
+      using storage_t = typename std::aligned_storage<sizeof(Holder<void>), std::alignment_of<Holder<void>>::value>::type;
+
+      const std::type_info *  my_type_info{ nullptr };
+      // Inplace storage to hold Holder<T>
+      storage_t               my_inplace_storage;
+
+      // Pointer to my_inplace_storage that holds Holder<T> 
+      const IHolder *  holder() const noexcept 
+      { 
+        [[gsl::suppress(type.1)]]  // warning C26490 : Don't use reinterpret_cast. (type.1)
+        return reinterpret_cast<const IHolder *>(&my_inplace_storage); 
+      }
+
+      template<typename T>
+      friend std::shared_ptr<T> any_shared_ptr_cast(any_shared_ptr const & anySharedPtr);
+
+      template<typename T>
+      friend std::optional<std::shared_ptr<T>> any_shared_ptr_cast(any_shared_ptr const * anySharedPtr) noexcept;
+
+    };
+
+    //-----------------------------------------------------------------------------------------------------
+    // Implementation
+
+    template<typename T>
+    any_shared_ptr::any_shared_ptr(std::shared_ptr<T> ptr) noexcept
+      : my_type_info( & typeid(HeldType<T>) )
+    {
+      ::new (&my_inplace_storage) Holder<T>(std::move(ptr));
+    }
+
+    [[gsl::suppress(type.6)]] //  warning C26495 : Variable 'xxx::ver_1::any_shared_ptr::my_inplace_storage' is uninitialized.Always initialize a member variable. (type.6)
+    inline any_shared_ptr::any_shared_ptr() noexcept
+    {
+      ::new (&my_inplace_storage) Holder<void>();
+    }
+
+    inline any_shared_ptr::~any_shared_ptr()
+    {
+      holder()->~IHolder();
+    }
+
+    inline any_shared_ptr::any_shared_ptr(any_shared_ptr const & other) noexcept
+      : my_type_info( other.my_type_info )
+    {
+      other.holder()->clone(&my_inplace_storage);
+    }
+
+    inline any_shared_ptr::any_shared_ptr(any_shared_ptr && other) noexcept
+      : my_type_info(other.my_type_info)
+    {
+      my_inplace_storage = other.my_inplace_storage;
+      other.reset();
+    }
+
+    inline any_shared_ptr& any_shared_ptr::operator=(any_shared_ptr const& other) noexcept
+    {
+      if (this != &other)
+      {
+        this->~any_shared_ptr();
+        my_type_info = other.my_type_info;
+        other.holder()->clone(&my_inplace_storage);
+      }
+      return *this;
+    }
+
+    inline any_shared_ptr& any_shared_ptr::operator=(any_shared_ptr && other) noexcept
+    {
+      if (this != &other)
+      {
+        this->~any_shared_ptr();
+        my_type_info = other.my_type_info;
+        my_inplace_storage = other.my_inplace_storage;
+        other.reset();
+      }
+      return *this;
+    }
+
+    inline void any_shared_ptr::swap(any_shared_ptr & other) noexcept
+    {
+      other = std::exchange(*this, std::move(other));
+    }
+
+    inline const std::type_info & any_shared_ptr::type() const noexcept
+    {
+      return my_type_info ? *my_type_info : typeid(void);
+    }
+
+    inline bool  any_shared_ptr::unique() const noexcept
+    {
+      return holder()->unique();
+    }
+
+    template <typename T>
+    std::shared_ptr<T> any_shared_ptr::cast(bool & cast_ok) const noexcept
+    {
+      std::shared_ptr<T> result;
+      if (has_value()) {
+        const IHolder * pholder{ holder() };
+        if (type() == typeid(HeldType<T>)) {
+          [[gsl::suppress(type.2)]] // warning C26491: Don't use static_cast downcasts. A cast from a polymorphic type should use dynamic_cast. (type.2)
+          result = static_cast<const Holder<T>*>(pholder)->my_ptr;
+          cast_ok = true;
+        }
+        else { // try an implicit up cast by throwing an exception
+          try {
+            pholder->try_up_cast_by_throwing();
+          }
+          catch (T* p) { // implicit up cast succeeded
+            result = std::static_pointer_cast<T>(pholder->make_shared_ptr_alias(p));
+            cast_ok = true;
+          }
+          catch (...) { // up cast failed
+          }
+        }
+      }
+      return result;
+    }
+
+    //-----------------------------------------------------------------------------------------------------
+
+    template<typename T>
+    std::shared_ptr<T> any_shared_ptr_cast(any_shared_ptr const & anySharedPtr)
+    {
+      bool is_cast_ok{ false };
+      const std::shared_ptr<T> result = anySharedPtr.template cast<T>(is_cast_ok);
+      if (is_cast_ok) {
+        return result;
+      }
+      throw bad_any_shared_ptr_cast();
+    }
+
+    template<typename T>
+    std::optional<std::shared_ptr<T>> any_shared_ptr_cast(any_shared_ptr const * anySharedPtr) noexcept
+    {
+      std::optional<std::shared_ptr<T>> result;
+      bool is_cast_ok{ false };
+      const std::shared_ptr<T> cast_result = anySharedPtr->template cast<T>(is_cast_ok);
+      if (is_cast_ok) {
+        result = cast_result;
+      }
+      return result;
+    }
+
+  } // namespace ver_1
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+
+} // namespace xxx {
+
+
+namespace std {
+
+  inline void swap(xxx::any_shared_ptr & lhs, xxx::any_shared_ptr & rhs)
+  {
+    lhs.swap(rhs);
+  }
+
+} // namespace std
---
+++ src/test/test_any_shared_ptr.cpp
@@ -0,0 +1,134 @@
+#include <any_shared_ptr.h>
+#include <type_traits>
+
+using namespace xxx;
+using namespace std;
+
+TEST(any_shared_ptr, empty)
+{
+  any_shared_ptr any;
+
+  ASSERT_FALSE(any.has_value());
+  
+  // Check the held type
+  ASSERT_EQ(any.type(), typeid(void));
+
+  // Test any_shared_ptr_cast throws bad cast error
+  EXPECT_THROW(any_shared_ptr_cast<void>(any), bad_any_shared_ptr_cast);
+  EXPECT_THROW(any_shared_ptr_cast<int>(any), bad_any_shared_ptr_cast);
+}
+
+TEST(any_shared_ptr, basic_cast)
+{
+  auto p1 = make_shared<int>(42);
+  any_shared_ptr any1{ p1 };
+
+  any_shared_ptr any2{ any1 };
+
+  ASSERT_TRUE(any1.has_value());
+  // Check the held type
+  ASSERT_EQ(any1.type(), typeid(shared_ptr<int>));
+  // Test casting to the held type
+  shared_ptr<int> p2 = any_shared_ptr_cast<int>(any1);
+  ASSERT_EQ(p1.get(), p2.get());
+  ASSERT_EQ(p1.get(), any_shared_ptr_cast<int>(any2).get());
+}
+
+TEST(any_shared_ptr, cv_promotion_violations)
+{
+  // Test casting from 'const' to 'non-const' fails
+  {
+    any_shared_ptr any{ make_shared<const int>(42) };
+
+    ASSERT_EQ(any.type(), typeid(shared_ptr<const int>));
+
+    EXPECT_THROW(any_shared_ptr_cast<int>(any), bad_any_shared_ptr_cast);
+    EXPECT_THROW(any_shared_ptr_cast<volatile int>(any), bad_any_shared_ptr_cast);
+  }
+  // Test casting from 'volatile' to 'non-volatile' fails
+  {
+    any_shared_ptr any{ make_shared<volatile int>(42) };
+
+    ASSERT_EQ(any.type(), typeid(shared_ptr<volatile int>));
+
+    EXPECT_THROW(any_shared_ptr_cast<int>(any), bad_any_shared_ptr_cast);
+    EXPECT_THROW(any_shared_ptr_cast<const int>(any), bad_any_shared_ptr_cast);
+  }
+  // Test casting from 'const volatile' to 'non-volatile-const' fails
+  {
+    any_shared_ptr any{ make_shared<const volatile int>(42) };
+
+    ASSERT_EQ(any.type(), typeid(shared_ptr<const volatile int>));
+
+    EXPECT_THROW(any_shared_ptr_cast<int>(any), bad_any_shared_ptr_cast);
+    EXPECT_THROW(any_shared_ptr_cast<const int>(any), bad_any_shared_ptr_cast);
+    EXPECT_THROW(any_shared_ptr_cast<volatile int>(any), bad_any_shared_ptr_cast);
+  }
+}
+
+
+TEST(any_shared_ptr, const_promotion)
+{
+  // Test non-const -> const & non-const cast
+  {
+    auto ptr = make_shared<int>(42);
+    any_shared_ptr anyPtr{ ptr };
+
+    EXPECT_NO_THROW(any_shared_ptr_cast<int>(anyPtr) );
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int>(anyPtr).get());
+    EXPECT_NO_THROW(any_shared_ptr_cast<int const>(anyPtr) );
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int const>(anyPtr).get());
+
+    EXPECT_NO_THROW(any_shared_ptr_cast<int>(anyPtr));
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int>(anyPtr).get());
+    EXPECT_NO_THROW(any_shared_ptr_cast<int const>(anyPtr));
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int const>(anyPtr).get());
+
+  }
+  // Test const -> const & non-const cast
+  // Note const -> non-const should throw bad_any_shared_ptr_cast
+  {
+    auto ptr = make_shared<int const>(42);
+    any_shared_ptr anyPtr{ ptr };
+
+    EXPECT_THROW(any_shared_ptr_cast<int>(anyPtr), bad_any_shared_ptr_cast);
+    EXPECT_NO_THROW(any_shared_ptr_cast<int const>(anyPtr));
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int const>(anyPtr).get());
+
+    EXPECT_THROW(any_shared_ptr_cast<int>(anyPtr), bad_any_shared_ptr_cast);
+    EXPECT_NO_THROW(any_shared_ptr_cast<int const>(anyPtr));
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int const>(anyPtr).get());
+  }
+}
+
+
+
+TEST(any_shared_ptr, VolatilePromotion)
+{
+  // Test non-volatile -> volatile & non-volatile cast
+  {
+    auto ptr = make_shared<int>(42);
+    any_shared_ptr anyPtr{ ptr };
+
+    EXPECT_NO_THROW(any_shared_ptr_cast<int>(anyPtr));
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int>(anyPtr).get());
+    EXPECT_NO_THROW(any_shared_ptr_cast<int volatile>(anyPtr));
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int volatile>(anyPtr).get());
+
+    EXPECT_NO_THROW(any_shared_ptr_cast<int>(anyPtr));
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int>(anyPtr).get());
+    EXPECT_NO_THROW(any_shared_ptr_cast<int const>(anyPtr));
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int volatile>(anyPtr).get());
+
+  }
+  // Test volatile -> volatile & non-volatile cast
+  // Note volatile -> non-volatile should throw bad_any_shared_ptr_cast 
+  {
+    auto ptr = make_shared<int volatile>(42);
+    any_shared_ptr anyPtr{ ptr };
+
+    EXPECT_THROW(any_shared_ptr_cast<int>(anyPtr), bad_any_shared_ptr_cast);
+    EXPECT_NO_THROW(any_shared_ptr_cast<int volatile>(anyPtr));
+    ASSERT_EQ(ptr.get(), any_shared_ptr_cast<int volatile>(anyPtr).get());
+  }
+}
